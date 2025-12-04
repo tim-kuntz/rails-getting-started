c0fb35a97afceb1b2fa173a0bc775b3d5950aee9
Tim Kuntz <timkuntz@gmail.com>
2025-12-03 21:49:45 -0600

    Rails Getting Started up to section 17

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+  include Authentication
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale]
+    locale = I18n.default_locale unless I18n.locale_available?(locale)
+    I18n.with_locale(locale, &action)
+  end
+end
